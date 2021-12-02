b99cbd75e6322e191ccdc4b53f0987ef0ec6ff98
Giuditta9255 <91844686+Giuditta9255@users.noreply.github.com>
2021-12-02 01:14:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+  int numero, conta=0;
+  cin>>numero;
+  primo(numero, 1, conta);
+  if(conta==0){
+    cout<<"numero primo";
+  }else{
+    cout<<"numero non primo";
+  }
   return 0;
 }
